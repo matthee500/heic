b657a5555a6ba50260eb814456c99e2f642c3595
matthee500 <ruanmatthee@protonmail.com>
2023-03-16 12:29:12 +0000

    made script more reliable

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
 import subprocess
 
-
-def convert():
-
-    directory = '.'
-
+def convert(directory='.'):
     for filename in os.listdir(directory):
         if filename.lower().endswith(".heic"):
-            print('Converting %s...' % os.path.join(directory, filename))
-            subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
-            continue
-
+            filepath = os.path.join(directory, filename)
+            print(f'Converting {filepath}...')
+            try:
+                subprocess.run(["magick", filepath, filepath[:-5] + '.jpg'], check=True)
+                print(f'Successfully converted {filepath}')
+            except subprocess.CalledProcessError:
+                print(f'Error converting {filepath}')
 
 if __name__ == '__main__':
     convert()
